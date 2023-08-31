b4556ac7eb9268f32db7864368d3b1a56084d18f
creep <creepcn@gmail.com>
2023-08-31 15:58:17 +0800

    feat(node): read file using stream

--- source/node/parse.js
+++ source/node/parse.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs/promises')
+const fs = require('node:fs')
 const { performance } = require('node:perf_hooks')
 
 const base = require('../base')
@@ -8,6 +8,23 @@ const { Context } = require('./context')
 
 const { BinaryStream } = base
 
+const readFile = (path) => new Promise((resolve) => {
+  const chunks = []
+  const stream = fs.createReadStream(path, { highWaterMark: 10 * 1024 * 1024})
+
+  stream.on('readable', () => {
+    let chunk;
+    while ((chunk = stream.read()) !== null) {
+      chunks.push(chunk)
+    }
+  })
+
+  stream.on('end', () => {
+    stream.close()
+    resolve(Buffer.concat(chunks))
+  })
+})
+
 async function parse(file) {
   const { Host } = require('../node')
 
@@ -15,12 +32,12 @@ async function parse(file) {
   const modelFactory = new ModelFactoryService(host)
 
   performance.mark('readFile')
-  const buffer = await fs.readFile(file)
+  const buffer = await readFile(file)
   performance.measure('readFile', 'readFile')
 
   performance.mark('parse')
 
-  const stream = new BinaryStream(new Uint8Array(buffer.buffer))
+  const stream = new BinaryStream(new Uint8Array(buffer))
   const context = new Context(host, file, null, stream)
   const model = await modelFactory.open(context)
 
